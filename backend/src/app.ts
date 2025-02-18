49d077f3bddf860834ef7e3f4cedcfa78e9f7716
Cristian Perez <cperezlecaros@gmail.com>
2025-02-18 14:16:51 -0700

    Install cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import authRouter from "./routes/auth.routes";
 import messageRouter from "./routes/message.routes";
 import userRouter from "./routes/user.routes";
@@ -13,6 +14,12 @@ const port = EnvConfiguration().port;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
