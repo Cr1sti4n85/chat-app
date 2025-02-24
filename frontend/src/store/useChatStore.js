e2d0e2958c7711bb41f4866219b1b2736033fefa
Cristian Perez <cperezlecaros@gmail.com>
2025-02-23 19:06:40 -0700

    Create useChatStore

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessasgesLoading: false,
+
+  async getUsers() {
+    set({ isUsersLoading: true });
+
+    try {
+      const res = await axiosInstance.get("/users");
+      set({ users: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  },
+
+  async getMessages(userId) {
+    set({ isMessasgesLoading: true });
+
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessasgesLoading: false });
+    }
+  },
+}));
