4d4a1f2de857600a6cf5f9bb697dcc679f216f35
Cristian Perez <cperezlecaros@gmail.com>
2025-02-23 19:28:05 -0700

    Create Home page and ChatPlaceholder component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import ChatPlaceholder from "../components/ChatPlaceholder";
+import Sidebar from "../components/Sidebar";
+import { useChatStore } from "../store/useChatStore";
+
 function HomePage() {
-  return <div></div>;
+  const { selectedUser } = useChatStore();
+  console.log(selectedUser);
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <ChatPlaceholder /> : <div>Chat</div>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
