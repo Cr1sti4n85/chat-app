d83bdbd3436237e234c541d7723c5ec6efe78a05
Cristian Perez <cperezlecaros@gmail.com>
2025-02-26 16:57:42 -0700

    Update useChatStore and create Message Input component

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getMessages } from "controllers/message.controller";
-import { protect } from "middleware/auth.middleware";
+import { getMessages, sendMessage } from "../controllers/message.controller";
+import { protect } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/:id", protect, getMessages);
+router.post("/send/:id", protect, sendMessage);
 
 export default router;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -32,6 +32,20 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.message);
     } finally {
       set({ isMessasgesLoading: false });
+    }
+  },
+  async sendMessage(messageData) {
+    const { selectedUser, messages } = get();
+
+    try {
+      const res = await axiosInstance.post(
+        `/messages/${selectedUser._id}`,
+        messageData
+      );
+
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.message);
     }
   },
   setSelectedUser(selectedUser) {
